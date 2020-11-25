37d7f0d39af371649c068fcb6a44b33d3c7c7f2f
Stincescu <sebistincescu@gmail.com>
2020-11-25 18:21:55 +0200

    - P3 STOP question

--- Week4P3/src/Main.java
+++ Week4P3/src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String []args){
+        ConnectableInterface[] obiecte = new ConnectableInterface[5];
+        Car masina = new Car();
+        Phone telefon = new Phone();
+        Refrigerator frigider = new Refrigerator();
+        Student student = new Student();
+
+        obiecte[0] = masina;
+        obiecte[1] = telefon;
+        obiecte[2] = frigider;
+        //obiecte[3] = student; // Have no idea
 
     }
 }
